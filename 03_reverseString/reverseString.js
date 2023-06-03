8a31f76b3b13ec922fe448c24a9b64112580c2bc
KZeeMe <102718492+KZeeMe@users.noreply.github.com>
2023-06-03 08:13:12 -0500

    string reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newString = []
+    for (let i = str.length; i >= 0; i--){
+        newString.push(str[i]);
+    }
+    return newString.join("");
 };
 
+console.log(reverseString('Hello there'));
 // Do not edit below this line
 module.exports = reverseString;
