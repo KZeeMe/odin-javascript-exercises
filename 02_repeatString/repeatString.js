42b3357f7e197fe74f7ed8038a1b86366e36d97e
KZeeMe <102718492+KZeeMe@users.noreply.github.com>
2023-06-03 07:14:13 -0500

    exercise 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let repeatedStr = "";
+    for (let i = 0; i < num; i++) {
+      repeatedStr += str;
+    }
+    return repeatedStr;
+  }
 };
 
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
