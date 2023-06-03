56ab499db86ff5d9226ffc2019f1d8c75ebc1dee
KZeeMe <102718492+KZeeMe@users.noreply.github.com>
2023-06-03 14:45:41 -0500

    solved multi conditional leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+console.log(leapYears(2000)); // expect true
+console.log(leapYears(1985)); // expect false
+console.log(leapYears(1600)); // expect true
+console.log(leapYears(1900)); // expect false
 // Do not edit below this line
 module.exports = leapYears;
