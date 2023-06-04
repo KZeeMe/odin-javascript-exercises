c3cab7b9d109988f47dd0a1fe7a1b06ea51469eb
KZeeMe <102718492+KZeeMe@users.noreply.github.com>
2023-06-04 04:41:32 -0500

    calculator functions exponential, factorial, multiplication, add, adding several numbers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let total = 0;
+  if (arr == []) return total;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n == 0 || n == 1) return 1;
+  let fact = 1;
+  while (n > 1) {
+    fact *= n;
+    n--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
