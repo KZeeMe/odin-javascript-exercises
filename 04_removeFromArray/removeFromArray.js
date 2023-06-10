499d5545c60aab268b128002425ba6251e292ac0
KZeeMe <102718492+KZeeMe@users.noreply.github.com>
2023-06-10 17:51:31 -0500

    revising solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,33 @@
-const removeFromArray = function() {
+// const removeFromArray = function (arr, ...item) {
+//   //let compareArr = [...item];
+//   let newArr = [];
+//   arr.forEach((item) => {
+//     if (!args.includes(item)) {
+//       newArr.push(item);
+//     }
+//     //return newArr;
+//   });
+//   return newArr;
+// };
 
+// let index = [];
+// let other_items = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === item) {
+//     index.push(item);
+//   } else {
+//     other_items.push(arr[i]);
+//   }
+// }
+// return other_items;
+//};
+
+const removeFromArray = function (arr, ...args) {
+  let newArr = arr.filter((item) => item != args.includes(item));
+  return newArr;
 };
+
+//console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
